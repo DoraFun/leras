eecd324400950c546ddef4ac1dc0b79bc8fc8043
DoraFun <lolovichlol606@gmail.com>
2022-07-14 02:04:01 +0500

    54

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,12 +13,24 @@ module.exports = {
     },
     gridTemplateColumns: {
       'fit2': "repeat(2, max-content)",
+      '4': "repeat(4, minmax(0, 1fr))",
+      '2': "repeat(2, minmax(0, 1fr))"
+    },
+    listStyleType: {
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      square: 'square',
+      roman: 'upper-roman',
     },
     extend: {
       colors: {
         "orange": "#EDB27B",
         "blue": '#6AC1C1',
         "dark": '#1A1A1A',
+        "semidark": '#272829',
+        "gray": '#A5A5A5',
+        "more-gray": '#CCCCCC',
       }
     },
   },
