a7c03dca96db924e534eea6d0236bd39fdb25343
DoraFun <lolovichlol606@gmail.com>
2022-07-15 02:58:36 +0500

    zaebalo ey bogy

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,7 @@ module.exports = {
     fontFamily: {
       'choco': ["TTChocolates-Regular"],
     },
-    gridTemplateRows: {
-      '7': "repeat(7, minmax(0, 1fr))",
-      'fit2': "repeat(2, max-content)",
-    },
-    gridTemplateColumns: {
-      'fit2': "repeat(2, max-content)",
-      '4': "repeat(4, minmax(0, 1fr))",
-      '2': "repeat(2, minmax(0, 1fr))"
-    },
-    listStyleType: {
-      none: 'none',
-      disc: 'disc',
-      decimal: 'decimal',
-      square: 'square',
-      roman: 'upper-roman',
-    },
+   
     extend: {
       colors: {
         "orange": "#EDB27B",
@@ -31,7 +16,24 @@ module.exports = {
         "semidark": '#272829',
         "gray": '#A5A5A5',
         "more-gray": '#CCCCCC',
-      }
+      },
+      gridTemplateRows: {
+        '7': "repeat(7, minmax(0, 1fr))",
+        'fit2': "repeat(2, max-content)",
+        'fit3': "repeat(3, max-content)",
+      },
+      gridTemplateColumns: {
+        'fit2': "repeat(2, min-content)",
+        'fit3': "repeat(3, max-content)",
+        'fit4': "repeat(4, max-content)",
+      },
+      listStyleType: {
+        none: 'none',
+        disc: 'disc',
+        decimal: 'decimal',
+        square: 'square',
+        roman: 'upper-roman',
+      },
     },
   },
   plugins: [],
